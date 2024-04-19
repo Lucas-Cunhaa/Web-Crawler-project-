f51c2e80fbba24d7bb8248a89be78befc0cc5d77
Lucas Cunha <lucasgalvao260@gmail.com>
2024-04-19 12:51:47 -0300

    Adding the structure of the project (before adding the wheater api

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const app = express()
+const routes = require('./routes')
+
+app.use(express.json())
+app.use(routes)
+
+app.listen(3090, () => { 
+    console.log('Executed on 3090 door')
+})
---
+++ routes.js
@@ -0,0 +1,7 @@
+const express = require('express') 
+const route = express.Router() 
+
+route.get('/weather/filter:', )
+route.get('/weather?citiy={city}')
+route.get('/weather/filter:')
+route.post('/weather')
