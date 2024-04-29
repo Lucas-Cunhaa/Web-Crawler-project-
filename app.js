a0c16117979a808103b27f519b9e375bcc41266c
Lucas Cunha <lucasgalvao260@gmail.com>
2024-04-29 10:56:11 -0300

    Changing door to aws

--- app.js
+++ app.js
@@ -5,6 +5,6 @@ const routes = require('./routes.js')
 app.use(express.json())
 app.use(routes)
 
-app.listen('launch-wizard-1', () => { 
-    console.log('Executed on 3090 door')
+app.listen(80, () => { 
+    console.log('Executed on 80door')
 })
